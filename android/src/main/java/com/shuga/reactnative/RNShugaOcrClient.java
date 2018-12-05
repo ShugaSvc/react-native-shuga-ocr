9c994ff8669349656a88b948560118015153c4b1
irvingdp <irvingdp@gmail.com>
2018-12-05 18:13:46 +0800

    fix client ocr can't detect text if not normal orientation issue

--- android/src/main/java/com/shuga/reactnative/RNShugaOcrClient.java
+++ android/src/main/java/com/shuga/reactnative/RNShugaOcrClient.java
@@ -2,7 +2,9 @@ package com.shuga.reactnative;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.Point;
+
 import android.support.annotation.NonNull;
 import android.util.Base64;
 
@@ -19,16 +21,96 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import android.support.media.ExifInterface;
+import android.util.Log;
+
+
 public class RNShugaOcrClient {
+
+    private int getOrientation(byte[] byteArray) {
+        ExifInterface exif;
+        int orientation = ExifInterface.ORIENTATION_NORMAL;
+        try {
+            InputStream inputStream = new ByteArrayInputStream(byteArray);
+            exif = new ExifInterface(inputStream);
+            orientation = exif.getAttributeInt( ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        return orientation;
+    }
+
+    private Bitmap rotate(int exifOrientation, Bitmap bitmap) {
+        final Matrix bitmapMatrix = new Matrix();
+        switch (exifOrientation) {
+            case 2:
+                bitmapMatrix.postScale(-1, 1);
+                break;
+            case 3:
+                bitmapMatrix.postRotate(180);
+                break;
+            case 4:
+                bitmapMatrix.postRotate(180);
+                bitmapMatrix.postScale(-1, 1);
+                break;
+            case 5:
+                bitmapMatrix.postRotate(90);
+                bitmapMatrix.postScale(-1, 1);
+                break;
+            case 6:
+                bitmapMatrix.postRotate(90);
+                break;
+            case 7:
+                bitmapMatrix.postRotate(270);
+                bitmapMatrix.postScale(-1, 1);
+                break;
+            case 8:
+                bitmapMatrix.postRotate(270);
+                break;
+            default:
+                break;
+        }
+
+        Bitmap transformedBitmap = Bitmap.createBitmap(
+                bitmap,
+                0,
+                0,
+                bitmap.getWidth(),
+                bitmap.getHeight(),
+                bitmapMatrix,
+                false
+        );
+
+        return transformedBitmap;
+    }
+
+
+    private Bitmap fixOrientation(Bitmap bitmap, byte[] bytes) {
+        try {
+            int exifOrientation = this.getOrientation(bytes);
+            if(exifOrientation != ExifInterface.ORIENTATION_NORMAL || exifOrientation != ExifInterface.ORIENTATION_UNDEFINED) {
+                return rotate(exifOrientation, bitmap);
+            }
+            return bitmap;
+        } catch (Exception ex) {
+            Log.e("RNShugaOcrClient", "Couldn't fixOrientation()", ex);
+            return  bitmap;
+        }
+
+    }
+
     // Method for React Native frontend, will invoke given callback upon success or error
     public void scanTextInImage(String base64String, final Callback successCallback, final Callback errorCallback) {
         byte[] bytes = Base64.decode(base64String, Base64.DEFAULT);
-
         //Convert to bitmap instead of calling .fromByteArray for base64 images, as for some reason, it'll throw "Invalid image data size" error
         Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        Bitmap transformedBitmap = this.fixOrientation(bitmap, bytes);
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(transformedBitmap);
 
         this.scanTextInImage(image, new RNShugaOcrClientAsyncResultDelegate() {
             @Override
@@ -77,6 +159,7 @@ public class RNShugaOcrClient {
     }
 
     private void scanTextInImage(FirebaseVisionImage image, final RNShugaOcrClientAsyncResultDelegate delegate) {
+
         FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
         detector.processImage(image)
                 .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
